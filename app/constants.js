fd8153126022932328c98ed6cd5a932b8a5a5cf8
yotamDvir <rocky_2@Yotams-MacBook-Air.local>
2016-04-04 14:21:45 +0300

    The markables
    Which markables appear and should they be marked by the user.

---
+++ app/constants.js
@@ -0,0 +1,13 @@
+export const
+	MARKABLES = [
+		{content: '1', shouldBeMarked: true},
+		{content: '2', shouldBeMarked: true},
+		{content: '3', shouldBeMarked: true},
+		{content: '4', shouldBeMarked: false},
+		{content: '5', shouldBeMarked: false},
+		{content: '6', shouldBeMarked: false},
+		{content: '7', shouldBeMarked: false},
+		{content: '8', shouldBeMarked: false},
+		{content: '9', shouldBeMarked: false},
+	];
+
