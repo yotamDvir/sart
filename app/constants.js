305c6b1bb7f16444398878ed84df3342f1028f72
yotamDvir <rocky_2@Yotams-MacBook-Air.local>
2016-04-04 14:23:26 +0300

    Keys and Intervals
    * Which keys perform actions.
    * What are the time intervals and the iteration counts.

--- app/constants.js
+++ app/constants.js
@@ -1,4 +1,12 @@
 export const
+	MARK_KEY 			= 32,
+	PHASE_KEY 			= 32,
+	SHOW_INT 			= 600,
+	HIDE_INT 			= 600,
+	ITERATIONS 			= 6,
+	ITERATIONS_INTRO 	= 2;
+
+export const
 	MARKABLES = [
 		{content: '1', shouldBeMarked: true},
 		{content: '2', shouldBeMarked: true},
