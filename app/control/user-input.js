c17ca82c0ad6db133f32569820e5bd1f13063137
yotamDvir <rocky_2@Yotams-MacBook-Air.local>
2016-04-04 14:53:04 +0300

    Control of user-input
    Listeners to MARK_KEY and PHASE_KEY.

---
+++ app/control/user-input.js
@@ -0,0 +1,45 @@
+import store from './store';
+import {show, mark, clear, advance} from './actions';
+import {
+	SAMPLE, TEST, FEEDBACK, DIRECTIONS,
+	PHASE_KEY, MARK_KEY,
+	SHOW_INT, HIDE_INT,
+	ITERATIONS, ITERATIONS_INTRO,
+} from '../constants';
+import randMark from '../helpers/random-markable';
+import ticker from './ticker';
+
+
+const phaser = function (phase) {
+	if (phase === FEEDBACK) {
+		store.dispatch(clear());
+	}
+	store.dispatch(show(randMark()));
+	store.dispatch(advance());
+	const isSample = store.getState().phase === SAMPLE;
+	ticker.reset();
+	const iter = isSample ? ITERATIONS_INTRO : ITERATIONS;
+	ticker.start((SHOW_INT+HIDE_INT)*iter);
+};
+
+const marker = function (phase) {
+	const currentMarkable = store.getState().xnumber.int;
+	store.dispatch(mark(currentMarkable));
+};
+
+const onKeyUp = function (e) {
+	const key = e.which;
+	if (key !== PHASE_KEY && key !== MARK_KEY) {
+		return undefined;
+	}
+	e.preventDefault();
+	let phase = store.getState().phase;
+	if (key === PHASE_KEY && [FEEDBACK, DIRECTIONS].indexOf(phase) >= 0) {
+		phaser(phase);
+	}
+	if (key === MARK_KEY && [SAMPLE, TEST].indexOf(phase) >= 0) {
+		marker(phase);
+	}
+};
+
+document.body.addEventListener('keyup', onKeyUp);
